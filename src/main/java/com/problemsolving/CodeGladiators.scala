7683ae48fcecc40132a7014778799547259559a1
hobbist <kapil.21.kulkarni21@gmail.com>
2022-05-11 20:09:11 +0530

    adding new classes

---
+++ src/main/java/com/problemsolving/CodeGladiators.scala
@@ -0,0 +1,85 @@
+package com.problemsolving
+
+object CodeGladiators extends App{
+//missleTime
+  findModalValue
+  def missleTime={
+    val launchTime= scala.io.StdIn.readLine().split(" ").map(x=>x.toInt)
+    val timespan=scala.io.StdIn.readLine().split(" ").map(x=>x.toInt)
+    val hitTime=Array.ofDim[Int](2)
+    val minutesAdd=launchTime(1)+timespan(1)
+    var hourAdd=launchTime(0)+timespan(0)
+    var hourCarry=0
+    if(minutesAdd>=60) {
+      hitTime(1) = minutesAdd - 60
+      hourCarry = 1
+    }else{
+      hitTime(1) = minutesAdd
+    }
+
+    hourAdd=hourAdd+hourCarry
+    if(hourAdd>=24){
+      hitTime(0)=hourAdd-24
+    }else{
+      hitTime(0)=hourAdd
+    }
+
+    println(hitTime.map(x=> if(x<10) "0"+x.toString else x).mkString(" "))
+  }
+
+  def buyGifts: Unit ={
+    val testCases=scala.io.StdIn.readLine().toInt
+    for(i<-0 until testCases){
+      val giftsToBuy=scala.io.StdIn.readLine().toInt
+      val numberofGifts=scala.io.StdIn.readLine().toInt
+      val gifts=scala.io.StdIn.readLine().split(" ").map(z=>z.toDouble).sorted
+      var minimumCost:Double=0
+      for(j<-0 until giftsToBuy){
+        minimumCost=minimumCost+gifts(j)
+      }
+      println(minimumCost.toString.replace(".0",""))
+    }
+  }
+
+  def mthLargest={
+    //o(mn)
+    val inputSize="7".toInt
+    val input="25 26 7 8 10 11 79".split(" ").map(_.toInt)
+    val m="3".toInt
+    val largest=Array.fill[Int](m)(Integer.MIN_VALUE)
+    for(i <- 0 until inputSize){
+      var temp=input(i)
+      for(j<- 0 until largest.size){
+        if(temp > largest(j)){
+          var t=largest(j)
+          largest(j)=temp
+          temp=t
+        }
+      }
+      println(largest(largest.size-1))
+    }
+  }
+
+  def evenOddWar={
+    val inputSize="7".toInt
+    val input="74 32 31 91 77 88 96 44 23".split(" ").map(_.toInt)
+    val map=scala.collection.mutable.Map[Int,Double](1->0,2->0)
+    for(ele <- input){
+      if(ele%2==0)map.put(1,map.get(1).get+ele)
+      else map.put(2,map.get(2).get+ele)
+    }
+    if(map.get(1).get > map.get(2).get) println("Even")
+    if(map.get(2).get > map.get(1).get) println("odd")
+    if(map.get(2).get == map.get(1).get) println("Tied")
+  }
+
+  def findModalValue={
+    val inputSize="7".toInt
+    val input="6 3 9 6 6 5 9 3".split(" ").map(_.toInt)
+    val map=scala.collection.mutable.Map.empty[Int,Double]
+    for(ele<- input){
+      map.put(ele,1 + map.getOrElse(ele,0.toDouble))
+    }
+    println(map.maxBy(x=>x._2)._1)
+  }
+}
--- src/main/java/com/problemsolving/HackerRankProblems.scala
+++ src/main/java/com/problemsolving/HackerRankProblems.scala
@@ -4,14 +4,17 @@ import java.io._
 import java.math._
 import java.security._
 import java.text._
+import java.util
 import java.util._
 import java.util.concurrent._
 import java.util.function._
 import java.util.regex._
 import java.util.stream._
+
 import scala.collection.immutable._
 import scala.collection.mutable._
 import scala.collection.concurrent._
+import scala.collection.mutable
 import scala.concurrent._
 import scala.io._
 import scala.math._
@@ -66,10 +69,83 @@ object HackerRankProblems extends App {
     }
   }
 
+  def minMaxProblem(): Unit ={
+    val arr=Array(1,2,3,4,5)
+    var minValue:Long=Int.MaxValue
+    var maxValue:Long=Int.MinValue
+    var sum:Long=0
+    for(i<- 0 until arr.length){
+      if(arr(i)< minValue) minValue=arr(i)
+      if(arr(i)>maxValue) maxValue=arr(i)
+      sum=sum+arr(i)
+    }
+    println(s"${sum-maxValue}  ${sum-maxValue}")
+  }
+
+  def tallestCandles={
+    val candles=Array(4,4,1,2,3)
+    var tallest=Int.MinValue
+    var tallestCount=1
+    candles.foreach(i=>{ if(i>tallest) { tallest=i;tallestCount=1} else if(i==tallest) tallestCount=tallestCount+1})
+    println(s"$tallest $tallestCount")
+  }
+
+  def gradingStudents(grades: Array[Int]): Array[Int] = {
+    var j=0
+    val ans=grades.map(i=> if(i>=38){
+      val nextMultipleDiff=5*((i/5)+1)-i
+      if(nextMultipleDiff<3) i+nextMultipleDiff
+      else i
+    }else i)
+    ans
+  }
+
+  def caesarCipher(s: String ="!m-rB`-oN!.W`cLAcVbN/CqSoolII!SImji.!w/`Xu`uZa1TWPRq`uRBtok`xPT`lL-zPTc.BSRIhu..-!.!tcl!-U", k: Int=62): String = {
+    val steppedDown=if(k>26) k%26 else k
+    val sb=new scala.collection.mutable.StringBuilder()
+    val alph=scala.collection.immutable.List(
+      'A','B','C','D','E','F','G','H','I',
+      'J','K','L','M','N','O','P','Q','R',
+      'S','T','U','V','W','X','Y','Z')
+    s.foreach(c=> {
+      var charToappend:Char = Char.MinValue
+      if(c.isLetter){
+        if(alph.indexOf(c.toUpper) + steppedDown > alph.size-1)
+          charToappend=alph(alph.indexOf(c.toUpper) + steppedDown - 26)
+        else charToappend=alph(alph.indexOf(c.toUpper) + steppedDown)
+      }else charToappend=c
+      if(c.isLower) charToappend=charToappend.toLower
+
+      sb.append(charToappend)
+    })
+    sb.toString()
+  }
+
+  def camelCase: Unit ={
+    val words="saveTheCamel"
+    var wordCount= if(!words.isEmpty) 1 else 0
+    for(c <-words){
+      if(c.isUpper) wordCount+=1
+    }
+  }
+
+  def isPanagram(s:String="We promptly judged antique ivory buckles for the prize"):String={
+    val m:scala.collection.mutable.Map[Char,Int]=scala.collection.mutable.Map.empty
+    var i=0
+    while(i < s.length && m.size<26){
+      if(s.charAt(i).isLetter) {
+        m.put(s.charAt(i).toLower, m.getOrElse(s.charAt(i).toLower, 0) + 1)
+      }
+      i+=1
+    }
+    if(m.size==26)"pangram" else "not pangram"
+  }
+
 
   val mat=Array(Array(11,2,4),Array(4,5,6),Array(10, 8, -12))
   val ar=Array(-4, 3, -9, 0, 4, 1)
   //println(diagonalDifference(mat))
   //plusMinus(ar)
-  staircase(6)
+  //staircase(6)
+  tallestCandles
 }
